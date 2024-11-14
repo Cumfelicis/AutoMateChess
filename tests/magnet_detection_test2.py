77ebec8c5bda03c628c8b311b360c537cb71d543
Felix Gross <121715997+Cumfelicis@users.noreply.github.com>
2024-11-14 13:19:34 +0100

    Update magnet_detection_test2.py
    finished to develop test script for magnet detection

--- tests/magnet_detection_test2.py
+++ tests/magnet_detection_test2.py
@@ -13,6 +13,29 @@ tolerance = 12
 data = [512 for _ in range(num_pins)]
 pins = [HalLPin(arduino, i) for i in range(num_pins)]
 
+
+piece_map = {
+  K: ,
+  Q: ,
+  R: ,
+  B: ,
+  N: ,
+  P: ,
+  k: ,
+  q: ,
+  r: ,
+  b: ,
+  n: ,
+  p: ,
+   # TODO: add curly braces
+
+def map_piece_to_voltage(voltage):
+   for i in range(voltage - tolerance, voltage + tolerance):
+     try:
+       return piece_map[i]
+    except Exception:
+        pass
+  
 def update_data():
   for i, x in enumerate(pins):
     data[i] = x.read()
@@ -30,10 +53,18 @@ def check_for_change(alt, new):
   return previous, updated
 
 
-def detect():
+def detect_movement():
   while True:
     previous, updated = check_for_change(data.copy(), update_data)
     if previous is not False and updated is not False:
       print(f'Piece moved from previous to updated') # add curly braces
-      
-  
+      detect_position()
+    time.sleep(1)
+
+
+def detect_position():
+  print([map_piece_to_voltage(i) for i in update_data()])
+
+  if __name__ == '__main__':
+    detect_movement()
+    
