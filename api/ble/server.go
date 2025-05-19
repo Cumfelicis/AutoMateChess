1ee6b947c1aa612139eea372e7f5d4dae56f9bb4
Cumfelicis <flix.gross@gmx.de>
2025-05-19 13:14:36 +0200

    debug

--- api/ble/server.go
+++ api/ble/server.go
@@ -43,7 +43,7 @@ func main() {
 				notifier = n
 				for !n.Done() {
 					time.Sleep(time.Second * 10)
-					sendFragmentedMessage(notifier, "Periodic server Message")
+					sendFragmentedMessage(notifier, "Periodic server Message eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
 				}
 			})
 			// Add and advertise
