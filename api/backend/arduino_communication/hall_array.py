9f005c011d5b9979e4c55723dbc367b6f87a1052
Cumfelicis <flix.gross@gmx.de>
2025-05-27 16:02:10 +0200

    debug

--- api/backend/arduino_communication/hall_array.py
+++ api/backend/arduino_communication/hall_array.py
@@ -65,7 +65,12 @@ class Array:
 
     def read(self, pos): # takes list as postion of the hall sensor to be read from
         sensor = 8 * pos[0] + pos[1] # transforms the input to fit the layout of the real hall-sensors
-        bin_sensor = f'{((sensor % 16) + 8) % 15:04b}'  
+        temp = sensor % 16
+        
+        if temp <= 7:
+            bin_sensor = f'{temp + 8:04b}'
+        else:
+            bin_sensor = f'{temp - 8:04b}'
         sensor_array = self.in_pins[3 - (sensor // 16)]
         for x, pin in enumerate(self.signal_pins[3 - (sensor // 16)]):
             pin.write(int(bin_sensor[x]))
