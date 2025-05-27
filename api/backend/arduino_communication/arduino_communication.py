1656c9dcc819ca50a7de7c03ac49d09f6e633229
Cumfelicis <flix.gross@gmx.de>
2025-05-28 01:06:55 +0200

    debug

--- api/backend/arduino_communication/arduino_communication.py
+++ api/backend/arduino_communication/arduino_communication.py
@@ -37,7 +37,7 @@ class Magnet:
     def __init__(self, board, relais_1, relais_2, relais_3):
         self.relais_1 = board.get_pin(f"d:{relais_1}:o")
         self.relais_2 = board.get_pin(f"d:{relais_2}:o")
-        self.relais_2.write(1)
+        self.relais_2.write(0)
         time.sleep(1)
         self.relais_3 = board.get_pin(f"d:{relais_3}:o")
 
@@ -46,34 +46,34 @@ class Magnet:
         self.set_polarity(polarity)
         
     def test(self):
-        self.relais_1.write(0)
-        self.relais_3.write(1)
-        self.relais_2.write(0)
+        self.relais_1.write(1)
+        self.relais_3.write(0)
+        self.relais_2.write(1)
 
     def off(self):
         print('start-off')
-        self.relais_1.write(0)
+        self.relais_1.write(1)
         time.sleep(1)
-        self.relais_2.write(1)
+        self.relais_2.write(0)
         print('off')
         
     def set_polarity(self, polarity):
         if polarity:
-            self.relais_1.write(0)
-            time.sleep(1)
-            self.relais_2.write(1)
-            time.sleep(1)
-            self.relais_3.write(0)
-            time.sleep(1)
             self.relais_1.write(1)
-        else:
-            self.relais_1.write(0)
             time.sleep(1)
-            self.relais_2.write(1)
+            self.relais_2.write(0)
             time.sleep(1)
             self.relais_3.write(1)
-            time.sleep(0)
+            time.sleep(1)
+            self.relais_1.write(0)
+        else:
+            self.relais_1.write(1)
+            time.sleep(1)
             self.relais_2.write(0)
+            time.sleep(1)
+            self.relais_3.write(0)
+            time.sleep(0)
+            self.relais_2.write(1)
 
 
 class Stepper:
