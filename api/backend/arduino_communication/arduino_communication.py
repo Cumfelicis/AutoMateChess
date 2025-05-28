b6e93a89f60f57e40ccc429cbb4a221a25218654
Cumfelicis <flix.gross@gmx.de>
2025-05-28 04:11:09 +0200

    debug

--- api/backend/arduino_communication/arduino_communication.py
+++ api/backend/arduino_communication/arduino_communication.py
@@ -50,22 +50,22 @@ class Magnet:
 
     def off(self):
         print('start-off')
-        self.relais_1.write(1)
+        self.relais_1.write(0)
         time.sleep(1)
         self.relais_2.write(1)
         print('off')
         
     def set_polarity(self, polarity):
         if polarity:
-            self.relais_1.write(1)
+            self.relais_1.write(0)
             time.sleep(1)
             self.relais_2.write(1)
             time.sleep(1)
             self.relais_3.write(1)
             time.sleep(1)
-            self.relais_1.write(0)
-        else:
             self.relais_1.write(1)
+        else:
+            self.relais_1.write(0)
             time.sleep(1)
             self.relais_2.write(1)
             time.sleep(1)
