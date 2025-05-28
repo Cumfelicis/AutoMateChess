aba3d8d1a465c546c9aaacad4ef16eb29b8d9fd3
Cumfelicis <flix.gross@gmx.de>
2025-05-28 15:37:51 +0200

    debug

--- api/backend/tests/test2.py
+++ api/backend/tests/test2.py
@@ -13,7 +13,7 @@ time.sleep(1)
 
 stepper_x = Stepper(5, 2, True, board=board_1, board_2=board_2, reference_pin=7, alternative_reference_pin=6)
 stepper_y = Stepper(6, 3, False, board=board_1, board_2=board_2, reference_pin=5)
-magnet = Magnet(board_2, 8, 9, 10)
+# magnet = Magnet(board_2, 8, 9, 10)
 multistepper = Multistepper()
 multistepper.add_stepper(stepper_x)
 multistepper.add_stepper(stepper_y)
